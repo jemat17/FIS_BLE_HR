7d447e46820746a4c6ecd2ad684ececc45aa2562
jemat17 <jemat17@student.sdu.dk>
2020-11-30 12:36:49 +0100

    Plot og bash

--- BLEHR/start.sh
+++ BLEHR/start.sh
@@ -1,3 +1,12 @@
-sudo service bluetooth restart
-python3 BLEHeartRateLogger.py
-python3 plot5.py
+#!/usr/bin/env bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+sudo service bluetooth restart 
+python3 BLEHeartRateLogger.py &
+python3 plot5.py &
+sleep 20
+xdg-open "http://127.0.0.1:8050/" &
+
+wait
